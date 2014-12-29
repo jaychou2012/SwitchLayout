3cd05b8a8269006c0ccb5b547edebcd916280db6
jaychou2012 <852041173@qq.com>
2014-12-29 14:48:18 +0800

    SwitchLayout源码
    SwitchLayout源码

---
+++ SwichLayoutLib/src/com/tandong/swichlayout/SwichLayoutInterFace.java
@@ -0,0 +1,13 @@
+package com.tandong.swichlayout;
+
+/**
+ * QQ 85204173
+ * 
+ * @author Tan Dong 2014.12.28
+ */
+public interface SwichLayoutInterFace {
+
+	public void setEnterSwichLayout();
+
+	public void setExitSwichLayout();
+}
